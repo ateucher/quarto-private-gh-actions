39f850b469234793d3117d14777dd783da801de7
Andy Teucher <andy.teucher@gmail.com>
2025-04-01 16:03:21 -0700

    Allow short

--- encrypt.R
+++ encrypt.R
@@ -16,7 +16,7 @@ staticryptR::staticryptr(
   files = "_site/",
   directory = ".",
   password = Sys.getenv("STATICRYPT_PASSWORD"),
-  short = FALSE, # set to TRUE if you want to use a short password
+  short = TRUE, # set to FALSE if you want to enforce a long password
   recursive = TRUE,
   template_title = "Password required for access",
   template_instructions = "Enter the password or contact example@email.com",
