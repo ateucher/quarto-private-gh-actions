1efbeda9ff67a459d3bdc769e002da4799ffbe28
Andy Teucher <andy.teucher@gmail.com>
2025-04-01 15:58:57 -0700

    Fallback function is failing, try without

--- encrypt.R
+++ encrypt.R
@@ -12,24 +12,12 @@ if (isTRUE(staticryptR::check_system())) {
   )
 }
 
-prompt_for_password <- function(prompt = "Enter a password: ") {
-  pwd <- if (interactive()) {
-    readline(prompt)
-  } else {
-    cat(prompt)
-    readLines("stdin", n = 1)
-  }
-  pwd
-}
-
 staticryptR::staticryptr(
   files = "_site/",
   directory = ".",
-  password = Sys.getenv("STATICRYPT_PASSWORD", prompt_for_password()),
+  password = Sys.getenv("STATICRYPT_PASSWORD"),
   short = FALSE, # set to TRUE if you want to use a short password
   recursive = TRUE,
-  template_color_primary = "#6667AB",
-  template_color_secondary = "#f9f9f3",
   template_title = "Password required for access",
   template_instructions = "Enter the password or contact example@email.com",
   template_button = "Access"
